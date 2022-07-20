a8f03130c15e16d4acdb7e7e2a03032a64c6f9a0
AshleyTavarez <94486969+AshleyTavarez@users.noreply.github.com>
2022-07-19 23:41:56 -0400

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Recetas.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        // GET: api/Category
+        private Recetas.RecetasEntities4 _context = new Recetas.RecetasEntities4();
+
+        public IEnumerable<Recetas.Category> Get()
+        {
+            var list = _context.Categories.ToList().Select(x => new Recetas.Category { Id_Categoria = x.Id_Categoria, Nombre = x.Nombre }).ToList();
+
+            return list.AsEnumerable();
+        }
+
+
+        // GET: api/Category/5
+        public HttpResponseMessage Get(int id)
+        {
+
+            var CateId = _context.Categories.ToList().Select(x => new Recetas.Category { Id_Categoria = x.Id_Categoria, Nombre = x.Nombre }).ToList().Find(x => x.Id_Categoria == id);
+
+            if (CateId != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, CateId);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+            }
+        }
+
+
+
+        // POST: api/Category
+        [HttpPost]
+        public IHttpActionResult Post(Category categories)
+        {
+            _context.Categories.Add(categories);
+            _context.SaveChanges();
+            return CreatedAtRoute("DefaultApi", new { id = categories.Id_Categoria }, categories);
+
+        }
+
+        // PUT: api/Category/5
+        [HttpPut]
+        [ResponseType(typeof(Category))]
+        public HttpResponseMessage PutCategory(int id, [FromBody] Category categories)
+        {
+            if (!ModelState.IsValid)
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+
+            using (var ctx = new Recetas.RecetasEntities4())
+            {
+                var existingCategory = ctx.Categories.Where(s => s.Id_Categoria == categories.Id_Categoria).FirstOrDefault<Category>();
+
+                if (existingCategory != null)
+                {
+                    existingCategory.Id_Categoria = categories.Id_Categoria;
+                    existingCategory.Nombre = categories.Nombre;
+
+                    ctx.SaveChanges();
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+                }
+
+                var CaId = _context.Categories.Find(id);
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, CaId);
+
+            }
+        }
+
+        // DELETE: api/Category/5
+        [HttpDelete]
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult Delete(int id)
+        {
+            Category category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return Ok(category);
+        }
+
+    }
+  
+}
+
---
+++ Controllers/FavoritesController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Recetas.Controllers
+{
+    public class FavoritesController : ApiController
+    {
+
+        private Recetas.RecetasEntities4 _context = new Recetas.RecetasEntities4();
+
+        // GET: api/Favorites
+        public IEnumerable<Recetas.Favorite> Get()
+        {
+            var list = _context.Favorites.ToList().Select(x => new Recetas.Favorite { Id_Fav = x.Id_Fav, Id_recipe = x.Id_recipe, Id_User = x.Id_User}).ToList();
+            return list.AsEnumerable();
+        }
+
+        // GET: api/Favorites/5
+        public HttpResponseMessage Get(int id)
+        {
+
+            var FavId = _context.Favorites.ToList().Select(x => new Recetas.Favorite { Id_Fav = x.Id_Fav, Id_recipe = x.Id_recipe }).ToList().Find(x => x.Id_Fav == id);
+
+            if (FavId != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, FavId);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+            }
+
+
+        }
+
+        // POST: api/Favorites
+        public IHttpActionResult Post(Favorite favorites)
+        {
+            _context.Favorites.Add(favorites);
+            _context.SaveChanges();
+            return CreatedAtRoute("DefaultApi", new { id = favorites.Id_Fav }, favorites);
+        }
+
+        // PUT: api/Favorites/5
+        [HttpPut]
+        [ResponseType(typeof(Favorite))]
+        public HttpResponseMessage PutIngretient(int id, [FromBody] Favorite favorites)
+        {
+            if (!ModelState.IsValid)
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+
+            using (var ctx = new Recetas.RecetasEntities4())
+            {
+                var existingFav = ctx.Favorites.Where(s => s.Id_Fav == favorites.Id_Fav).FirstOrDefault<Favorite>();
+
+                if (existingFav != null)
+                {
+                    existingFav.Id_Fav = favorites.Id_Fav;
+                    existingFav.Id_recipe = favorites.Id_recipe;
+                    existingFav.Id_User = favorites.Id_User;
+                    
+                    
+
+                    ctx.SaveChanges();
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Code or Member Not Found");
+                }
+
+                var FavId = _context.Favorites.Find(id);
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, FavId);
+
+            }
+        }
+
+        // DELETE: api/Favorites/5
+        [HttpDelete]
+        [ResponseType(typeof(Favorite))]
+        public IHttpActionResult Delete(int id)
+        {
+            Favorite favorites = _context.Favorites.Find(id);
+
+            if (favorites == null)
+            {
+                return NotFound();
+
+            }
+
+            _context.Favorites.Remove(favorites);
+            _context.SaveChanges();
+
+            return Ok(favorites);
+        }
+    }
+}
